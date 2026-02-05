4ae0ab5f4a5478bf40c6f3290c423a832420a5c9
masaaki010922 <elesag110@gmail.com>
2026-02-05 11:01:24 +0900

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,16 +1,16 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Saral Theme'
+export const SITE_TITLE = 'いとうのオンライン住居'
 export const SITE_DESCRIPTION =
-	'A simple theme for personal blog sites, created for Astro framework'
+	'伊藤雅哲の趣味用WEBページ。'
 
 // If you deploy to a subdirectory, set the `WEBSITE_BASE` here.
 // e.g. '/blog' if you are deploying to example.com/blog
 // Leave it as an empty string if you are deploying to a root domain (e.g. example.com)
-export const SITE_BASE = '/saral-theme-astro'
+export const SITE_BASE = ''
 
-export const SocialLinks = [
+export const SocialLinks = [https://x.com/_masaaki_ito
 	{
 		name: 'GitHub',
 		url: 'https://github.com/yashjawale',
